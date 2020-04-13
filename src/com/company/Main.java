5e076d12abf2576f334db54f31f9413352492840
cyric77 <cyric77@gmail.com>
2020-04-13 13:27:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i = 0;
+        String someString = "Some string";
+        System.out.println("This is a number: " + i + " and this is a string: " + someString);
+    }
+}
